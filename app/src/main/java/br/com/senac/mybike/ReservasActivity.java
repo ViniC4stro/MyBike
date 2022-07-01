b22f5f9d0132ffb31dd438f199dcc385aa6d7634
ViniC4stro <vinicius.castro2104@gmail.com>
2022-07-01 14:30:22 -0300

    Criando o botão de reserva (Float Action Button)

--- app/src/main/java/br/com/senac/mybike/ReservasActivity.java
+++ app/src/main/java/br/com/senac/mybike/ReservasActivity.java
@@ -3,12 +3,27 @@ package br.com.senac.mybike;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ReservasActivity extends AppCompatActivity {
+    FloatingActionButton fabReservar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reservas_layout);
+
+        fabReservar = findViewById(R.id.fabReservar);
+
+        fabReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Reserva Realizada!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
